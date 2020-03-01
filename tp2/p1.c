b80c9684a6fc4435e64fce0076adadee008f939a
TsarkFC <digasromao2000@gmail.com>
2020-03-01 18:26:17 +0000

    Finished first question

--- tp2/p1.c
+++ tp2/p1.c
@@ -9,29 +9,32 @@
 
 int main(void)
 {
- struct termios term, oldterm;
- int i;
- char pass[MAX_PASSWD_LEN+1], ch, echo = '*';
+    struct termios term, oldterm;
+    int i;
+    char pass[MAX_PASSWD_LEN+1], ch, echo = '*';
 
- write(STDOUT_FILENO, "\nPassword? ", 11);
+    write(STDOUT_FILENO, "\nPassword? ", 11);
 
- tcgetattr(STDIN_FILENO, &oldterm);
- term = oldterm;
- term.c_lflag &= ~(ECHO | ECHOE | ECHOK | ECHONL | ICANON);
- tcsetattr(STDIN_FILENO, TCSAFLUSH, &term);
+    tcgetattr(STDIN_FILENO, &oldterm);
+    term = oldterm;
+    term.c_lflag &= ~(ECHO | ECHOE | ECHOK | ECHONL | ICANON);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &term);
 
- i=0;
- while (i < MAX_PASSWD_LEN && read(STDIN_FILENO, &ch, 1) && ch != '\n') {
-  pass[i++] = ch;
-  write(STDOUT_FILENO, &echo, 1);
- }
- pass[i] = 0;
+    i=0;
 
- tcsetattr(STDIN_FILENO, TCSANOW, &oldterm);
- write(STDOUT_FILENO, "\n\nPassword: ", 12);
- write(STDOUT_FILENO, pass, strlen(pass));
- write(STDOUT_FILENO, "\n", 1);
+    //READ PASSWORD AND WRITE *
+    while (i < MAX_PASSWD_LEN && read(STDIN_FILENO, &ch, 1) && ch != '\n') {
+        pass[i++] = ch;
+        write(STDOUT_FILENO, &echo, 1);
+    }
+    pass[i] = 0;
 
- return 0;
+    //WRITE PASSWORD AT THE END
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldterm);
+    write(STDOUT_FILENO, "\n\nPassword: ", 12);
+    write(STDOUT_FILENO, pass, strlen(pass));
+    write(STDOUT_FILENO, "\n", 1);
+
+    return 0;
 }
 
