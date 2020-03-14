e7dffc1ffbe5283d164e734041b3f1d20f1b7d4e
TsarkFC <digasromao2000@gmail.com>
2020-03-14 20:34:29 +0000

    Exercise 7

---
+++ tp3/p7.c
@@ -0,0 +1,19 @@
+// PROGRAMA p7.c
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char *argv[]){
+    char prog[20];
+    sprintf(prog,"%s.c",argv[1]);
+    printf("Program %s has been compiled\n", argv[1]);
+    execlp("gcc","gcc",prog,"-Wall","-o",argv[1],NULL); // Compiles code from argv[1].c
+
+    /***EXTRA -> executes code from argv[1].c
+    char p[20] = "./";
+    strcat(p, argv[1]);
+    execlp(p, p, NULL);
+    */
+    exit(0);
+} 
