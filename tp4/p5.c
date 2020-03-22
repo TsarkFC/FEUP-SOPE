8868a7c8e4c515f9465cea86ecc41ad0f0c88d96
TsarkFC <digasromao2000@gmail.com>
2020-03-22 00:37:29 +0000

    [tp4] Exercise 5

--- tp4/p5.c
+++ tp4/p5.c
@@ -7,12 +7,20 @@
 #include <signal.h>
 
 
+pid_t pid;
+int i, n, status;
+
+void handler(int signo){
+    printf("Handler!\n");
+    pid = wait(&status);
+    printf("PID: %d\n", pid);
+    //Always 'forgets' one children!
+}
+
 int main(void){
-    pid_t pid;
-    int i, n, status;
 
     struct sigaction action;
-    action.sa_handler = SIG_IGN;
+    action.sa_handler = handler;
     sigemptyset(&action.sa_mask);
     action.sa_flags = 0;
 
@@ -25,7 +33,7 @@ int main(void){
         pid=fork(); 
         if (pid == 0){
             printf("CHILD no. %d (PID=%d) working ... \n",i,getpid());
-            sleep(i*7); // child working ...
+            sleep(15); // child working ...
             printf("CHILD no. %d (PID=%d) exiting ... \n",i,getpid());
             exit(0);
         }
@@ -33,12 +41,11 @@ int main(void){
 
     for (i=1 ;i<=4; i++ ) {
         printf("PARENT: working hard (task no. %d) ...\n",i);
-        n=20; 
+        n=15; 
         
         while((n=sleep(n))!=0);
         
         printf("PARENT: end of task no. %d\n",i);
-        printf("PARENT: waiting for child no. %d ...\n",i);
     }
     exit(0);
 }
